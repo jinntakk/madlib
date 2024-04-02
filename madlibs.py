bb9b7af47ab90f29855ee6bac5d3b97e85fbeb12
jinntakk <takjinwoo@gmail.com>
2024-04-02 00:19:29 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# word1 = "Hello my name "
+# word2 = "is Jin"
+
+# print(f"{word1}{word2}")
+
+adj = input("Write Adjective: ")
+verb1 = input("Write verb: ")
+verb2 = input("Write verb: ")
+famous_person = input("Write famous person: ")
+
+madlib = f"Computer programming is so {adj}. lt makes me so excited all the time because \
+l love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
